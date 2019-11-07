cbabdf798d3ab3fe83a9f10067dc6cf6664e00e2
范带川 <zxc10731962@qq.com>
2019-11-07 15:13:33 +0800

    init

--- quna/src/main.js
+++ quna/src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import rem from './config/rem'
+//引入rem。js实现rem布局，可以省略。js
+import reset from './config/reset.css'
+import fastclick from 'fastclick'
+//引入package中的插件直接输入名字即可，不需。/
+fastclick.attach(document.body)
+    //减少移动端点击事件延迟
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
