d0ff39125b52904ef858a1d81a9ac6aee0309420
范带川 <zxc10731962@qq.com>
2019-11-07 17:11:16 +0800

    homeheader

---
+++ quna/src/pages/Home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <homeheader :city="city">  
+        </homeheader>  
+    </div>
+</template>
+
+<script>
+import homeheader from "./components/homeheader"
+export default {
+    name:"Home",
+    data(){
+        return{
+            city:"秦皇岛"
+        }
+    },
+    components:{
+        homeheader
+    }
+}
+</script>
---
+++ quna/src/pages/Home/components/homeheader.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+        <div class="header">
+            <div class="left" @click="goback()">
+                <i class="icon iconfont icon-fanhui" ></i>
+            </div>
+            <div class="center">
+                <div class="put">
+                    <i class="icon iconfont icon-fangdajing fdj"></i>
+                    <p class="p1">输入城市/景点/游玩主题</p>
+                </div>
+            </div>
+            <div class="right">
+                {{city}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+   name:"homeheader",
+   props: ["city"],
+   data(){
+       return{
+           
+       }
+   },
+   methods:{
+       goback:function(){
+           this.$router.go(-1)
+       }
+   }
+}
+</script>
+<style>
+    .header{
+        width: 100%;
+        height: .44rem;
+        background-color: #00bcd4;
+    }
+    .left,.center,.right{
+        height: .44rem;
+        float: left;
+    }
+    .left{
+        width: .385rem;
+        text-align: center;
+        padding-top: 0.05rem;
+    }
+    .center{
+        height: .37rem;
+        width: 2.55rem;
+        padding-top: 0.07rem;
+    }
+    .center .put{
+        width: 2.55rem;
+        height: .24rem;
+        background-color: #fff;
+        border-radius: .02rem;
+        padding-top: 0.06rem;
+    }
+    .center .put i{
+        
+        float: left;
+    }
+    .center .put .p1{
+        font-size: .12rem;
+        display: block;
+        width: 1.5rem;
+        float: left;
+        
+    }
+    .right{
+        width: .7rem;
+        font-size: .21rem;
+        padding-top: .1rem;
+        margin-left: .1rem;
+    }
+</style>
